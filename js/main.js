e1e1ffe3914e03d8a166d3278d749040a28ed1df
BRUNO CRISTIANO LOOS E SILVA <bruno.silva18@alunos.sc.senac.br>
2025-08-20 18:37:04 -0300

    adicionado opções de interação no post funcao curtir sendo implementada

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
+let comment = false
+let liked = false
 // Você pode adicionar interações aqui futuramente
 document.querySelector(".login-form")?.addEventListener("submit", (e) => {
   e.preventDefault();
   window.location.href = "feed.html"; // Redireciona após login
 });
+
+function LikePost(postOption) {
+  if (!liked) {
+    postOption.src = "https://img.icons8.com/nolan/64/filled-like.png"
+    liked = true
+    return
+  }
+    postOption.src = "https://img.icons8.com/nolan/64/like.png"
+    liked = false
+}
+
+function CommentPost(postOption){
+    if (!comment) {
+    postOption.src = "https://img.icons8.com/pulsar-gradient/50/chat-message.png"
+    comment = true
+    return
+  }
+    postOption.src = "https://img.icons8.com/pulsar-gradient/50/chat-message.png"
+    comment = false
+}
